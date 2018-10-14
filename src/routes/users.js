becf68d1ecee6aef9c5f1f8b85ba1e116d482529
Nathan Carlos Santos Lima <32680502+NathanCarlos@users.noreply.github.com>
2018-10-14 20:14:54 -0300

    Atualizando uma das rotas

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,6 +5,7 @@ const User = require('../models/user');
 const router = express.Router();
 const usersController = new UsersController(User);
 
+router.get('/findAll', async(req,res) => usersController.get(req,res));
 router.post('/create/', (req, res) => usersController.create(req,res));
 router.put('/update/:id', (req, res) => usersController.update(req,res));
 router.get('/authentication/:email/:senha', (req,res) => usersController.authentication(req,res));
